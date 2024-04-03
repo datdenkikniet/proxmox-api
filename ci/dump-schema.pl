df8e30ed1367b295112f7269b085dc8cf2c67b12
datdenkikniet <jcdra1@gmail.com>
2024-04-03 21:46:58 +0200

    Add CI files

---
+++ ci/dump-schema.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use PVE::RESTHandler;
+use PVE::API2;
+use JSON;
+   
+  
+my $tree = PVE::RESTHandler::api_dump_remove_refs(PVE::RESTHandler::api_dump('PVE::API2'));
+  
+print to_json($tree, {pretty => 1, canonical => 1});
+  
+exit(0);
