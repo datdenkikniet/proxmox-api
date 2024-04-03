77b10fee7ba8b9aa4e545e37a39cc71e5b699b04
datdenkikniet <jcdra1@gmail.com>
2024-04-03 21:54:10 +0200

    1 != true

--- ci/setup.sh
+++ ci/setup.sh
@@ -14,4 +14,4 @@ mkdir -p $(dirname $inst)
 echo "deb http://download.proxmox.com/debian/pve $distro pve-no-subscription" > $inst
 wget https://enterprise.proxmox.com/debian/proxmox-release-$distro.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-$distro.gpg
 
-install proxmox-ve || 1
+install proxmox-ve || true
