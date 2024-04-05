cc1479f96fac40ccd838e40118e57e9e64c2753c
datdenkikniet <jcdra1@gmail.com>
2024-04-05 19:28:43 +0200

    Squashed commit of the following:
    commit 088c03093def15c0649fc89bdd5c71887fbb2be3
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:25:51 2024 +0200
    
        There
    
    commit feceb39d163d6adbed3a823f50b84d9f34020ffc
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:16:51 2024 +0200
    
        Cleanup
    
    commit a5ce4dc85c0f22e029a463c01011022b40c579e0
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:11:15 2024 +0200
    
        Testy
    
    commit c4bcfce6104c1eeec7ef1b04484d34ecca11f777
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:08:16 2024 +0200
    
        Typo
    
    commit 4b03f1ea03a25bcbb2267c67a82903dd645aa91c
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:06:26 2024 +0200
    
        Cleanup
    
    commit b114ff27112cdceced1f5c2af7e385ab1af4c228
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 19:02:58 2024 +0200
    
        Add some more beans
    
    commit 60c8eb51db2b1bf301c53a9440c90a3b380b4886
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:55:32 2024 +0200
    
        Must be true
    
    commit f486eb564e72022e5561af86c1c96f30583863f1
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:51:09 2024 +0200
    
        Ha ha!
    
    commit 3417b1c72259233df4bce0913c06efa8df49d066
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:42:54 2024 +0200
    
        Goodies
    
    commit 1e5bb12a9920be0e7f4283b98a933f33e23dd8c5
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:38:03 2024 +0200
    
        Add metadata
    
    commit 83432fc6ef91652d3204f05ddc09a917dff57356
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:27:53 2024 +0200
    
        Fix!
    
    commit 895051b04761fcada54576bc2a731e4fb0a9222d
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:22:50 2024 +0200
    
        Fix
    
    commit 4de11a7de30c99eb16a8355a31c2ba4d5c8f04ac
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:10:20 2024 +0200
    
        Rename
    
    commit c8fbfa1ac43e87a9555d3cc8abb3355ab1f285fa
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 18:09:43 2024 +0200
    
        Heh
    
    commit 05d3fd9f12c28bf0cde40c0748c0ecccbc018f28
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 17:58:58 2024 +0200
    
        CI stuff
    
    commit 7ab2bce5717cb84982288a227a3d40206a635d70
    Author: datdenkikniet <jcdra1@gmail.com>
    Date:   Fri Apr 5 17:58:55 2024 +0200
    
        Fixup

---
+++ ci/get-metadata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+metadata=$(cargo --offline metadata --format-version 1 --no-deps)
+
+if [ -z "$metadata" ]; then
+    echo "Metadata was empty?" >&2
+    exit 1
+fi
+
+output=$(echo "$metadata" | jq --raw-output ".packages[] | select(.name == \"proxmox-api\") | .metadata.proxmox.\"$1\"")
+
+if [[ -z "$output" || "$output" = "null" ]]; then
+    echo "Could not find requested metadata!" >&2
+    exit 1
+fi
+
+echo "$output"
