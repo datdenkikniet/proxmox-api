e4855dcc2196e6a3a0fea518696d803bba38da26
datdenkikniet <jcdra1@gmail.com>
2024-04-06 09:38:06 +0200

    Explicitly reexport this

--- proxmox-api/src/types/mod.rs
+++ proxmox-api/src/types/mod.rs
@@ -1,11 +1,21 @@
 mod bool;
-pub use bool::*;
+pub use bool::{
+    deserialize_bool, deserialize_bool_optional, serialize_bool, serialize_bool_optional, Bool,
+};
 
 mod integer;
-pub use integer::*;
+pub use integer::{
+    deserialize_int, deserialize_int_optional, serialize_int, serialize_int_optional, Integer,
+};
+
+mod mac_addr;
+pub use mac_addr::MacAddr;
 
 mod number;
-pub use number::*;
+pub use number::{
+    deserialize_number, deserialize_number_optional, serialize_number, serialize_number_optional,
+    Number,
+};
 
 mod vmid;
-pub use vmid::*;
+pub use vmid::VmId;
