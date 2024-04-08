b427b34fdfbaba8b5180d003c36779b8f6ece661
datdenkikniet <jcdra1@gmail.com>
2024-04-08 18:43:38 +0200

    Clean up formatted string

--- proxmox-api/src/types/formatted_string.rs
+++ proxmox-api/src/types/formatted_string.rs
@@ -1,31 +1,22 @@
-use std::str::FromStr;
-
-use serde::{de::Error, Deserializer};
-
 pub enum Output<O, I> {
     Single(O),
     Iter(I),
 }
 
-pub fn get_keys_or_default_value<'de, D, O, E>(
-    value: &str,
-) -> Result<Output<O, impl Iterator<Item = Result<(&str, &str), D::Error>>>, D::Error>
+pub fn get_keys_or_default_value<'str, O>(
+    value: &'str str,
+) -> Result<Output<O, impl Iterator<Item = Result<(&str, &str), String>>>, O::Error>
 where
-    O: FromStr<Err = E>,
-    D: Deserializer<'de>,
-    E: core::fmt::Display,
+    O: TryFrom<&'str str>,
 {
     // TODO: escape strings and stuff
     let is_multi_kvs = value.split_once(',').is_some() || value.split_once('=').is_some();
 
     let output = if !is_multi_kvs {
-        Output::Single(FromStr::from_str(value.trim()).map_err(D::Error::custom)?)
+        Output::Single(TryFrom::try_from(value.trim())?)
     } else {
         let iter = value.split(',').map(|v| {
-            let (k, v) = v
-                .split_once('=')
-                .ok_or_else(|| "missing key or value")
-                .map_err(D::Error::custom)?;
+            let (k, v) = v.split_once('=').ok_or_else(|| "missing key or value")?;
             Ok((k.trim(), v.trim()))
         });
 
@@ -38,24 +29,21 @@ where
 #[cfg(test)]
 mod test {
 
-    use std::str::FromStr;
-
-    use serde::{de::Error, Deserialize};
+    use serde::{Deserialize, Serialize};
 
     use super::get_keys_or_default_value;
 
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+    #[serde(try_from = "&str", into = "String")]
     pub struct Test {
         name: String,
     }
 
-    impl<'de> Deserialize<'de> for Test {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: serde::Deserializer<'de>,
-        {
-            let value = <&str>::deserialize(deserializer)?;
-            let values = get_keys_or_default_value::<D, _, _>(value)?;
+    impl TryFrom<&str> for Test {
+        type Error = String;
+
+        fn try_from(value: &str) -> Result<Self, Self::Error> {
+            let values = get_keys_or_default_value(value).unwrap();
 
             let iter = match values {
                 super::Output::Single(name) => return Ok(Test { name }),
@@ -65,36 +53,28 @@ mod test {
             let mut name = None;
 
             for value in iter {
-                let (k, v) = value.map_err(D::Error::custom)?;
+                let (k, v) = value?;
                 match k {
                     "name" => {
                         if name.is_some() {
-                            return Err(D::Error::custom("Duplicate key 'name'"));
+                            return Err("Duplicate key 'name'".into());
                         }
-                        name = Some(FromStr::from_str(v).map_err(D::Error::custom)?)
+                        name = Some(TryFrom::try_from(v).unwrap())
                     }
-                    key => return Err(D::Error::custom(format!("Unknown key {key}"))),
+                    key => return Err(format!("Unknown key {key}")),
                 }
             }
 
-            let name = name
-                .ok_or_else(|| "Missing required key 'name'")
-                .map_err(D::Error::custom)?;
+            let name = name.ok_or_else(|| "Missing required key 'name'".to_string())?;
 
             Ok(Test { name })
         }
     }
 
-    #[test]
-    pub fn from_default() {
-        let data = r#""name""#;
-        let unwrapped: Test = serde_json::from_str(data).unwrap();
-        assert_eq!(
-            unwrapped,
-            Test {
-                name: "name".into()
-            }
-        );
+    impl From<Test> for String {
+        fn from(value: Test) -> Self {
+            todo!()
+        }
     }
 
     #[test]
